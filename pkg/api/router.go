233f20082fe5635e5d393b5c3d37ee8433c9bf84
NicoBarragan <nbarragan01@gmail.com>
2022-10-07 01:49:06 -0300

    feat(router.go): initialized db, passed it as params to handler and also created person and passed in post func

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -1,21 +1,45 @@
 package api
 
 import (
+	"fmt"
+
+	"github.com/darchlabs/api-example/pkg/person"
+	"github.com/darchlabs/api-example/pkg/storage"
 	"github.com/julienschmidt/httprouter"
 )
 
 type response struct {
-	Data interface{} `json:"data,omitempty"`
-	Meta interface{} `json:"meta,omitempty"`
+	Data  interface{} `json:"data,omitempty"`
+	Meta  interface{} `json:"meta,omitempty"`
 	Error interface{} `json:"error,omitempty"`
 }
 
 func NewRouter() *httprouter.Router {
 	// initialize router
 	router := httprouter.New()
-	
-	// list persons
-	router.GET("/api/v1/persons", listPersonsHandler()) 
+
+	// Open db
+	db, err := storage.New("storage.db")
+
+	if err != nil {
+		fmt.Println("Bad db opening %w", err)
+		return nil
+	}
+
+	// Initialize storage with leveldb db
+	s := person.New(db)
+
+	/* list persons */
+	// Set a route for the func that lists persons in the s db
+	router.GET("/api/v1/persons", listPersonsHandler(s))
+	/* add persons */
+	// Create person p var
+	p := &person.Person{
+		Name: "Nico",
+		Age:  20,
+	}
+	// Set a route for the func that adds p persons to s db
+	router.POST("/api/v1/persons", addPersonsHandler(s, p))
 
 	return router
 }
