f446b19ac49868b06572d857afd6d043327fe427
NicoBarragan <nbarragan01@gmail.com>
2022-10-07 01:48:16 -0300

    refactor(handler.go): updated storage and person params and logic of funcs

--- pkg/api/handler.go
+++ pkg/api/handler.go
@@ -8,20 +8,37 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func listPersonsHandler() func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func listPersonsHandler(s *person.Storage) func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		// set headers
 		w.Header().Set("Content-Type", "application/json")
 
-		// list persons
-		// TODO(ca): get persons from database
-		// pp := make([]*person.Person, 0)
-		// pp = append(pp, person.NewPerson("nico", 20))
-		// pp = append(pp, person.NewPerson("camilo", 33))
+		pp, err := s.GetPersons()
+		if err != nil {
+			return
+		}
 
-		pp := []*person.Person{
-			person.NewPerson("nico", 20),
-			person.NewPerson("camilo", 33),
+		// get values to response
+		res := response{
+			Data: pp,
+		}
+
+		// prepare response to api
+		if err := json.NewEncoder(w).Encode(res); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func addPersonsHandler(s *person.Storage, p *person.Person) func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		// set headers
+		w.Header().Set("Content-Type", "application/json")
+
+		pp, err := s.NewPerson(*p)
+		if err != nil {
+			return
 		}
 
 		// get values to response
