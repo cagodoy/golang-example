d0fb9ca4343e806ee03c8e44051e30b1d4f88c53
NicoBarragan <nbarragan01@gmail.com>
2022-10-13 18:26:30 -0300

    feat(main.go): imported godotenv for getting env values

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,26 +4,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/darchlabs/api-example/pkg/api"
 	"github.com/darchlabs/api-example/pkg/storage"
 	personstorage "github.com/darchlabs/api-example/pkg/storage/person"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	// load env
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	fmt.Println("port: ", port)
-	// 	log.Fatalf("invalid env PORT value")
-	// }
-	port := "8080"
+	// load env values
+	godotenv.Load(".env")
+
+	// get env values
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatalf("invalid env PORT value")
+	}
+
+	dbPath := os.Getenv("DB_PATH")
+	if dbPath == "" {
+		log.Fatalf("invalid db path")
+	}
 
 	// open db
-	db, err := storage.New("storage.db")
+	db, err := storage.New(dbPath)
 	if err != nil {
-		log.Fatalf("Bad db opening %s", err) // fatal kills
+		log.Fatalf("Bad db opening %s", err)
 	}
 
 	// intialize person storage
