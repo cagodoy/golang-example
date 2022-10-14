99ed1054a72c973955cb5995330d29531a3fc56b
NicoBarragan <nbarragan01@gmail.com>
2022-10-14 12:39:11 -0300

    feat(router.go): updated ps interface responses, created handler, handlerRes and handlerCTX structs, and created handleFunc as http decorator over the handler's responses

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -17,7 +17,7 @@ type response struct {
 
 // Define handler's response
 type handlerRes struct {
-	Payload    string
+	Payload    interface{}
 	HttpStatus int
 	err        error
 }
@@ -29,7 +29,7 @@ type handlerCtx struct {
 	r  http.Request
 }
 
-type handler func(c *handlerCtx) handlerRes
+type handler func(c *handlerCtx) *handlerRes
 
 // Define ps methods
 type PersonStorage interface {
@@ -45,41 +45,50 @@ func NewRouter(ps PersonStorage) *httprouter.Router {
 	// initialize router
 	router := httprouter.New()
 
-	/// HERE IS THE ERROR: I can't pass the func as parameter for the decorator
+	// Instance handlers
+	listPersonsHandler := NewListPersonsHandler(ps)
+	getPersonHandler := NewGetPersonHandler(ps)
+	createPersonHandler := NewCreatePersonHandler(ps)
+	updatePersonHandler := NewUpdatePersonHandler(ps)
+	deletePersonHandler := NewDelPersonHandler(ps)
+
+	/* Create routes with the handle func receiving handler func's and &ps as params */
+	// Set a route for the func that lists persons in the s storage
+	router.GET("/api/v1/persons", HandleFunc(listPersonsHandler.Invoke, &ps))
+
 	// Set a route for the func that get p persons from s db by the id
-	// router.GET("/api/v1/person", HandleFunc(getPersonHandler, &ps))
+	router.GET("/api/v1/person", HandleFunc(getPersonHandler.Invoke, &ps))
+
+	// Set a route for the func that adds p persons to s db
+	router.POST("/api/v1/persons", HandleFunc(createPersonHandler.Invoke, &ps))
+
+	// Set a route for the func that adds p persons to s db
+	router.PUT("/api/v1/persons", HandleFunc(updatePersonHandler.Invoke, &ps))
+
+	// Set a route for delete a person from s db by the id
+	router.DELETE("/api/v1/person", HandleFunc(deletePersonHandler.Invoke, &ps))
 
 	return router
 }
 
+// Func that receives the returns from handlers and creates an http response
 func HandleFunc(fn handler, ps *PersonStorage) func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		ctx := handlerCtx{ps: *ps, w: w, r: *r}
-		handlerRes := fn(&ctx)
-		payload, statusCode, err := handlerRes.Payload, handlerRes.HttpStatus, handlerRes.err
 
+		ctx := &handlerCtx{ps: *ps, w: w, r: *r}
+		handlerRes := fn(ctx)
+
+		payload, statusCode, err := handlerRes.Payload, handlerRes.HttpStatus, handlerRes.err
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		res := response{
+			Meta: map[string]interface{}{"status_code": statusCode},
 			Data: payload,
-			Meta: statusCode,
 		}
 
 		json.NewEncoder(w).Encode(res)
 	}
 }
-
-// Set a route for the func that lists persons in the s storage
-// router.GET("/api/v1/persons", HandleFunc(listPersonsHandler()))
-
-// Set a route for the func that adds p persons to s db
-// router.PUT("/api/v1/persons", updatePersonHandler)
-
-// // Set a route for the func that adds p persons to s db
-// router.POST("/api/v1/persons", createPersonsHandler)
-
-// // Set a route for delete a person from s db by the id
-// router.DELETE("/api/v1/person", delPersonHandler)
