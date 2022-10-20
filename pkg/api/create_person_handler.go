5624353e810b4fcba379a9172f135115decd1b1f
NicoBarragan <nbarragan01@gmail.com>
2022-10-20 18:03:32 -0300

    feat(create_person_handler.go): updated CreatePersonHandler struct, using directly a PersonStorage type

--- pkg/api/create_person_handler.go
+++ pkg/api/create_person_handler.go
@@ -7,18 +7,15 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-type CreatePersonHandlerInputs struct {
-	Name string `validate:"required"`
-	Age  int64  `validate:"required"`
-}
-
 type CreatePersonHandler struct {
 	storage PersonStorage
+	// Person  *person.Person `json:"person"`
 }
 
 func NewCreatePersonHandler(ps PersonStorage) *CreatePersonHandler {
 	return &CreatePersonHandler{
 		storage: ps,
+		// Person:  &person.Person{},
 	}
 }
 
@@ -27,20 +24,23 @@ func (cp CreatePersonHandler) Invoke(ctx *handlerCtx) *handlerRes {
 	validate := validator.New()
 	validate.Struct(ctx.ps)
 
-	// prepare struct for body
+	// // Define structure for parse body
+	// body := &person.Person{}
+
+	// Define structure for parse body
 	body := &struct {
 		Person *person.Person `json:"person"`
 	}{}
 
-	// parse body using json decoder
+	// Parse body
 	decoder := json.NewDecoder(ctx.r.Body)
 	err := decoder.Decode(&body)
 	if err != nil {
 		return &handlerRes{err.Error(), 500, err}
 	}
 
-	// created person in storage
-	created, err := cp.storage.Create(body.Person)
+	// create a person in storage
+	created, err := ctx.ps.Create(body.Person)
 	if err != nil {
 		return &handlerRes{"Error creating person", 500, err}
 	}
